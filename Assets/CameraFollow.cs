04e1e6b2f91409261a6267994557cb32fac9fd2e
richinarm <60946200+richinarm@users.noreply.github.com>
2022-06-03 01:54:05 +0000

    Added Forward back controls for animation and speed limit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public PlayerMovement playerMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerMovement = FindObjectOfType<PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3 (playerMovement.transform.position.x, playerMovement.transform.position.y + 4, playerMovement.transform.position.z - 10);
+    }
+}
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Rigidbody playerbody;
+    public Rigidbody playerbody;
+    public Animator animator;
     int mouseSpeed = 100;
-    int playerSpeed = 5;
+    public int playerSpeed = 5;
 
     float horizontal;
     float vertical;
@@ -14,10 +15,16 @@ public class PlayerMovement : MonoBehaviour
     float xRotation;
     float yRotation;
 
+    bool isMovingForward;
+    int forwardSpeed = 5;
+    int backwardSpeed = 3;
+    bool isMovingBackward;
+
     // Start is called before the first frame update
     void Start()
     {
         playerbody = GetComponent<Rigidbody>();
+        animator = GameObject.Find("Player").transform.Find("Michelle").GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -26,17 +33,57 @@ public class PlayerMovement : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
-        var mouseLeftRight = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
-        var mouseUpDown = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
+        //var mouseLeftRight = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime;
+        //var mouseUpDown = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
 
-        yRotation += mouseLeftRight;
+        //yRotation += mouseLeftRight;
 
         playerbody.transform.rotation = Quaternion.Euler(0, yRotation, 0);
         MovePlayer();
+        PlayAnimation();
     }
 
     public void MovePlayer()
     {
-        transform.localPosition += (transform.right * horizontal + transform.forward * vertical) * playerSpeed * Time.deltaTime; 
+        if (isMovingForward)
+        {
+            playerSpeed = forwardSpeed;
+        }
+
+        else
+        {
+            playerSpeed = backwardSpeed;
+        }
+
+        transform.localPosition += (transform.right * horizontal + transform.forward * vertical) * playerSpeed * Time.deltaTime;
+
     }
+
+    public void PlayAnimation()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            print("W is pressed");
+            animator.SetBool("MoveForward", true);
+            isMovingForward = true;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.W))
+        {
+            animator.SetBool("MoveForward", false);
+            isMovingForward = false;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("W is pressed");
+            animator.SetBool("MoveBackward", true);
+        }
+
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            animator.SetBool("MoveBackward", false);
+        }
+    }
+
 }
